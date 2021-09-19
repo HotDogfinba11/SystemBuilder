de98c39df77226e2a7daa0344d4f1b143eda4f9e
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-09-19 12:13:19 +0100

    Project: Creation of SettingsView
    To have a view callable by SettingsView() preferences definition.

---
+++ SystemBuilder/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  SettingsView
+//
+//  Created by Oliver Harrison on 19/09/2021.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
