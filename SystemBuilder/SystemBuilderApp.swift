f7aa8daa88eac166a6ab6711a20c0b982debb2ce
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-09-19 11:21:43 +0100

    Initial Commit

---
+++ SystemBuilder/SystemBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemBuilderApp.swift
+//  SystemBuilder
+//
+//  Created by Oliver Harrison on 19/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SystemBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
