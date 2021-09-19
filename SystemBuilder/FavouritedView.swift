918fd249da6ae0861580cac25a9bc35a5c078292
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-09-19 11:47:18 +0100

    FavouritedView: PCList array
    For use in a NavigationView list, defining elements that are to be added.

--- SystemBuilder/FavouritedView.swift
+++ SystemBuilder/FavouritedView.swift
@@ -16,6 +16,13 @@ struct PCType: Identifiable {
 }
 
 struct FavouritedView: View {
+    @State var PCList = [
+        PCType(id: 0, name: "Nephew's PC", type: "Gaming", icon: "discord", price: "£700"),
+        PCType(id: 1, name: "Grandad's PC", type: "Gaming", icon: "discord", price: "£1500"),
+        PCType(id: 2, name: "Daughter's PC", type: "Desktop", icon: "apple", price: "£350"),
+        PCType(id: 3, name: "Staff PC", type: "Video editing", icon: "video", price: "£800"),
+    ]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
