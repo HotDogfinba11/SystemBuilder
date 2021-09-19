bde4745b86f41e99e067a4a600ac2719960bc4ff
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-09-19 11:37:47 +0100

    ContentView: Provide NavigationView collapse toggle
    Allowing the collapse of the NavigationView to maximise view for content.

--- SystemBuilder/ContentView.swift
+++ SystemBuilder/ContentView.swift
@@ -20,6 +20,15 @@ struct ContentView: View {
                     }
                 }
                 .frame(minWidth: 150)
+                .toolbar {
+                    ToolbarItem(placement: .automatic) {
+                        Button(action: {
+                            NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+                        }) {
+                            Image(systemName: "sidebar.left")
+                        }
+                    }
+                }
             }
         }
     }
