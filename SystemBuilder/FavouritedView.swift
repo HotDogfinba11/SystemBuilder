7e866efad34b6b62e7bf7068c26bc3a2c2646915
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-09-23 18:34:10 +0100

    FavouritedView: Renaming of PCType name
    Increases the applicable use cases that require similar hardware.

--- SystemBuilder/FavouritedView.swift
+++ SystemBuilder/FavouritedView.swift
@@ -20,7 +20,7 @@ struct FavouritedView: View {
         PCType(id: 0, name: "Nephew's PC", type: "Gaming", icon: "discord", price: "£700"),
         PCType(id: 1, name: "Grandad's PC", type: "Gaming", icon: "discord", price: "£1500"),
         PCType(id: 2, name: "Daughter's PC", type: "Desktop", icon: "apple", price: "£350"),
-        PCType(id: 3, name: "Staff PC", type: "Video editing", icon: "video", price: "£800"),
+        PCType(id: 3, name: "Staff PC", type: "Rendering", icon: "video", price: "£800"),
     ]
     
     var body: some View {
