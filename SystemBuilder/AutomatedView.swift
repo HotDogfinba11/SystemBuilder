7bf56f005e63aa5e14e81102b855637ca103f4cc
HotDogfinba11 <38323818+HotDogfinba11@users.noreply.github.com>
2021-12-03 16:19:04 +0000

    AutomatedView: Provide TabView
    TabView selection for switching between build type predefinitions.

--- SystemBuilder/AutomatedView.swift
+++ SystemBuilder/AutomatedView.swift
@@ -9,18 +9,32 @@ import SwiftUI
 import PythonKit
 
 struct AutomatedView: View {
-    var body: some View {
-        Button(action: test) {
-            Text("Test")
-        }
-    }
-    func test() {
-        print(Python.versionInfo.description)
-    }
+       var body: some View {
+           TabView {
+               GenerationView()
+                   .tabItem {
+                       Text("Sound Effects")
+                   }
+               GenerationView()
+                   .tabItem {
+                       Text("Output")
+                   }
+               GenerationView()
+                   .tabItem {
+                       Text("Input")
+                   }
+           }
+       }
 }
 
 struct AutomatedView_Previews: PreviewProvider {
     static var previews: some View {
         AutomatedView()
+    }
+}
+
+struct GenerationView: View {
+    var body: some View {
+        Text("build")
     }
 }
