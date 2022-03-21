5d0bec239cfbae55801cdec2858f110f199ed7ee
HotDogfinba11 <oliverharrison03@gmail.com>
2022-03-21 21:05:02 +0000

    Project: Comment current code

--- SystemBuilder/ContentView.swift
+++ SystemBuilder/ContentView.swift
@@ -8,28 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var ShowingView = true
+    @State private var ShowingView = true // Variable for forcing the view to open by default
     
     var body: some View {
-        NavigationView {
-            VStack {
-                List {
-                    Section(header: Text("System Builder")) {
-                        NavigationLink(destination: AutomatedView(), isActive: $ShowingView){
-                            Label("Build", systemImage: "macpro.gen3")
+        NavigationView { // Side menu contents
+            VStack { // Vertical stack for elements on top of each other
+                List { // Interactable list to hide sub items
+                    Section(header: Text("System Builder")) { // Top-level list name
+                        NavigationLink(destination: AutomatedView(), isActive: $ShowingView){ // Default view to render when opening the program
+                            Label("Build", systemImage: "macpro.gen3") // 
                         }
                         NavigationLink(destination: FavouritedView()) {
                             Label("Favourites", systemImage: "heart")
                         }
                     }
                 }
-                .frame(minWidth: 150)
-                .toolbar {
+                .frame(minWidth: 150) // Forcing user-adjustable width to a minimum of 150 pixels
+                .toolbar { // Create toolbar inside of NavigationView
                     ToolbarItem(placement: .automatic) {
                         Button(action: {
-                            NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil)
+                            NSApp.keyWindow?.firstResponder?.tryToPerform(#selector(NSSplitViewController.toggleSidebar(_:)), with: nil) // Standard implementation of collapsable NavigationView
                         }) {
-                            Image(systemName: "sidebar.left")
+                            Image(systemName: "sidebar.left") // SF symbol to represent collapsing function
                         }
                     }
                 }
--- SystemBuilder/SettingsView.swift
+++ SystemBuilder/SettingsView.swift
@@ -9,26 +9,26 @@ import SwiftUI
 
 struct SettingsView: View {
     var body: some View {
-        TabView {
+        TabView { // Creates clickable tabs
             Form {
-                Section(header: Text("Retailers"),
-                        footer: Text("Disabing retailers will prevent the automatic PC builder from using said retailer.")) {
-                    Toggle(isOn: .constant(true),
+                Section(header: Text("Retailers"), // First header
+                        footer: Text("Disabing retailers will prevent the automatic PC builder from using said retailer.")) { // Description text
+                    Toggle(isOn: .constant(true), // Interactable toggle, forced to on until functionality added
                            label: {
-                        Text("Amazon")
+                        Text("Amazon") // Text aligned with the toggle, indicating which source is related
                     })
                 }
-                Section(header: Text("About"),
-                        footer: Text("A-level programming project")) {
-                    Link("Source code is available on Github", destination: URL(string: "https://github.com/HotDogfinba11/SystemBuilder")!)
-                    Label("Version 0", systemImage: "info.circle")
+                Section(header: Text("About"), // Second header
+                        footer: Text("A-level programming project")) { // Description text
+                    Link("Source code is available on Github", destination: URL(string: "https://github.com/HotDogfinba11/SystemBuilder")!) // Standard text made blue due to hyperlink, linking to source code
+                    Label("Version 0", systemImage: "info.circle") // Current version with SF symbol
                 }
             }
-            .tabItem {
-                Label("Settings", systemImage: "gear")
+            .tabItem { // Adds any successive Label functions as a tab
+                Label("Settings", systemImage: "gear") // Clickable tab with hard-coded string and SF symbol
             }
         }
-        .frame(width: 500, height: 250)
+        .frame(width: 500, height: 250) // Pixel dimensions for SettingsView, hard-coded and not user adjustable
     }
 }
 
